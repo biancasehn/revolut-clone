55d2b786c6337f9e45f436cb3bbb7498fc717e31
Bianca Sehn <sehnbiancarafaela135@gmail.com>
2021-10-24 22:21:14 +0200

    hero banner layout to change on scroll

--- script.js
+++ script.js
@@ -8,15 +8,23 @@ const heroCta = document.querySelector("#hero-cta")
 const heroCtaSticky = document.querySelector(".sticky-cta")
 const lastMainSection = document.querySelector("#last-section")
 
+const heroBanner = document.querySelector("#hero-img-desktop")
+
 hideLocationContainer.addEventListener("click", () => 
     locationContainer.style.display = "none")
 
 countrySelector.addEventListener("click", () =>
     dropdown.classList.toggle("dropdown-displayed"))
 
+
+const bottomHeroBanner = heroBanner.getBoundingClientRect().bottom
+
 window.onscroll = () => {
     (heroCta.getBoundingClientRect().y < 0) && 
     (lastMainSection.getBoundingClientRect().y - window.innerHeight > 0) ? 
         heroCtaSticky.classList.add("sticky-cta-active")
         : heroCtaSticky.classList.remove("sticky-cta-active")
+
+    heroBanner.style.opacity = `calc(${heroBanner.getBoundingClientRect().bottom}/${bottomHeroBanner})`;
+    heroBanner.style.transform = `scale(calc(1 + 0.2 / ${heroBanner.getBoundingClientRect().bottom} * 100))`;
 }
